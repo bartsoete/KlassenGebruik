3566e739322cff8151eceeccd76e3d62d2a0fc46
Bart Soete <soeteba@gmail.com>
2017-07-09 22:50:34 +0200

    Een Test

---
+++ KlassenGebruik/Autos.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Voertuigen;
+
+namespace KlassenGebruik
+{
+    /// <summary>
+    /// Interaction logic for Autos.xaml
+    /// </summary>
+    public partial class Autos : Window
+    {
+        public Autos()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Opvullen van de comboBoxen
+
+           foreach (string s in Enum.GetNames(typeof(Auto.Kleur)))
+            {
+                cmbKleur.Items.Add(s);
+            } 
+            foreach (string s in Enum.GetNames(typeof(Auto.Merken)))
+            {
+                cmbMerk.Items.Add(s);
+            }
+
+            // Netjex : Eerste item Selecteren
+            cmbKleur.SelectedIndex = 0;
+            cmbMerk.SelectedIndex = 0;
+
+        }
+
+        private void btnVoegToe_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                int snelheidMax = int.Parse(txtMaxSnelheid.Text);
+                int plaatsenAuto = int.Parse(txtPlaatsen.Text);
+                Auto nieuweWagen = new Auto((Auto.Merken)Enum.Parse(typeof(Auto.Merken),cmbMerk.SelectedItem.ToString()),(Auto.Kleur)Enum.Parse(typeof(Auto.Kleur),cmbKleur.SelectedItem.ToString()),plaatsenAuto, snelheidMax);
+                lstAutos.Items.Add(nieuweWagen);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Kan niet invoegen, foutieve ingave \nReden :\t" + ex.Message, "Foutmelding");
+            }
+        }
+    }
+}
---
+++ Voertuigen/Auto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voertuigen
+{
+    public class Auto
+    {
+        public enum Kleur { Groen, Oranje, Rood, Awart }
+        public enum Merken { BMW, Volkswagen, Ford, Mercedes }
+        public Kleur KleurAuto { get; set; }
+        public int AantalPlaatsen { get; set; }
+        public int MaximumSnelheid { get; private set; }
+        public int ActueleSnelheid { get; private set; }
+        public Merken Merk { get; set; }
+
+
+        // Constructor aangemaakt bij het intiantiëren van een wagen dienen deze velden minimaal ingegeven
+        public Auto(Merken merk, Kleur kleur, int aantalPlaatsen, int maxSnelheid)
+        {
+            Merk = merk;
+            KleurAuto = kleur;
+            MaximumSnelheid = maxSnelheid;
+            AantalPlaatsen = aantalPlaatsen;
+        }
+
+        public void Verhoogsnelheid(int waarde)
+        {
+            if (ActueleSnelheid < MaximumSnelheid)
+            { 
+            ActueleSnelheid += waarde;
+            }
+        }
+
+        public void VerlaagSnelheid(int waarde)
+        {
+            if (ActueleSnelheid > 0)
+            { 
+            ActueleSnelheid -= waarde;
+            }
+        }
+
+        // Weergeven van de Auto met een override ToString() => Netjes weergeven in de Listbox
+        public override string ToString()
+        {
+            return Merk + "(" + KleurAuto + ")"; 
+        }
+    }
+}
